bc9fbdf3ea361bf936dfb8a65361862f56b2d71d
Luiz Henrique <luiz.hse@hotmail.com>
2018-03-28 14:11:18 -0300

    Cria controller de teste

---
+++ src/main/kotlin/com/concrete/kotlinapi/controllers/HelloController.kt
@@ -0,0 +1,13 @@
+package com.concrete.kotlinapi.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun helloKotlin(): String {
+        return "hello world"
+    }
+}
