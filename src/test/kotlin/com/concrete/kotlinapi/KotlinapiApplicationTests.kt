3f1bb14494631c5bb6abc9f0d1ab7ac8f5ea3dc1
Luiz Henrique <luiz.hse@hotmail.com>
2018-03-28 13:28:37 -0300

    Adiciona Estrutura Inicial do Projeto

---
+++ src/test/kotlin/com/concrete/kotlinapi/KotlinapiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.concrete.kotlinapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
